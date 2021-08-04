f8cc91beed83a94b0b753765aba9172fde458ef6
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-08-04 17:05:28 +0200

    realized there was more to day1

--- challenges/2020/day1.js
+++ challenges/2020/day1.js
@@ -7,8 +7,8 @@ import { inputs } from '../../index.js';
  * and then multiply those two numbers together.
  */
 
-// look for 2 eles matching the specific sum
-const findSumArr = (inputArr, sum = 2020) => {
+// look for 2 eles matching the specified sum
+const findSumOf2 = (inputArr, sum = 2020) => {
   // iterating over Arr to find the matching elements
   for (let i = 0; i < inputArr.length; i++) {
     // parse String ele to integer
@@ -25,12 +25,49 @@ const findSumArr = (inputArr, sum = 2020) => {
   return 0;
 };
 
+// look for 3 eles matching the specified sum
+const findSumOf3 = (inputArr, sum = 2020) => {
+  // iterating over Arr to find the matching elements
+  for (let i = 0; i < inputArr.length; i++) {
+    // parse String ele to integer
+    const ele1 = parseInt(inputArr[i]);
+    for (let n = 0; n < inputArr.length; n++) {
+      // skip to next iteration so we dont get same element
+      if (n === i) {
+        continue;
+      }
+      const ele2 = parseInt(inputArr[n]);
+      for (let k = 0; k < inputArr.length; k++) {
+        const ele3 = parseInt(inputArr[k]);
+        // same as above
+        if (k === i || k === n) {
+          continue;
+        }
+        // looking for our result
+        if (ele1 + ele2 + ele3 === sum) {
+          // returning obejct
+          return { ele1, ele2, ele3 };
+        }
+      }
+    }
+  }
+  return 0;
+};
+
 // day 1 default function to find result
 export default function () {
   //getting our input as string and splitting into array
   const inputArr = inputs.year20day1.split('\n');
   //calling function to find the correct values
-  const values = findSumArr(inputArr);
+  const values = findSumOf2(inputArr);
+  const values2 = findSumOf3(inputArr);
   console.log(`Two values that sum to 2020: ${values.ele1} + ${values.ele2}`);
   console.log(`Product from those: ${values.ele1 * values.ele2}`);
+  console.log('--------------------------------');
+  console.log(
+    `Three values that sum to 2020: ${values2.ele1} + ${values2.ele2} + ${values2.ele3}`
+  );
+  console.log(
+    `Product from those: ${values2.ele1 * values2.ele2 * values2.ele3}`
+  );
 }
