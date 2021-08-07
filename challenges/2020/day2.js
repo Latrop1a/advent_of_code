ac6cefbec86ea7b33a9fd6c0650efb3afdda8824
Latrop <32779759+Latrop1a@users.noreply.github.com>
2021-08-07 08:55:41 +0200

    found bug. used .substring instead of .substr

--- challenges/2020/day2.js
+++ challenges/2020/day2.js
@@ -1,5 +1,7 @@
 // gets the input file
 import { inputs } from '../../index.js';
+// for testing
+import assert from 'assert';
 
 /**
  * 1-3 a: abcde
@@ -16,7 +18,7 @@ const parsePasswordString = passwordString => {
   const splitArr = passwordString.split(' ');
   // example [ '5-17', 'z:', 'knzcjlgjlkvglrsqzwt' ]
   // refining output to use in checkPassword function
-  const pwRangeArr = splitArr[0].split('-');
+  let pwRangeArr = splitArr[0].split('-').map(ele => parseInt(ele));
   const charToCheck = splitArr[1].substring(0, 1);
   const password = splitArr[2];
 
@@ -33,15 +35,15 @@ const checkPasswordValidity1 = parsedPwObj => {
   );
 };
 
-// checks if the password is valid according to the rules set in part1
+// checks if the password is valid according to the rules set in part2
 const checkPasswordValidity2 = parsedPwObj => {
   let counter = 0;
-  const pw = parsedPwObj.password,
-    char = parsedPwObj.charToCheck,
-    posArr = parsedPwObj.pwRangeArr;
-  // checking if char is at the substring position1
-  if (pw.substring(posArr[0] - 1, 1) === char) counter++;
-  if (pw.substring(posArr[1] - 1, 1) === char) counter++;
+  //destructuring obj for better readability
+  const { password: pw, charToCheck: char, pwRangeArr: posArr } = parsedPwObj;
+  // checking if char is at the substring position
+  if (pw.substr(posArr[0] - 1, 1) === char) counter++;
+  if (pw.substr(posArr[1] - 1, 1) === char) counter++;
+
   // returning true if we have exactly one occurence of char at the given spots
   return counter === 1;
 };
@@ -51,18 +53,14 @@ export default function () {
   let validPwCount1 = 0;
   let validPwCount2 = 0;
   // getting input as string and splitting into array
-  const inputArr = inputs.year20day2.split('\n');
+  const inputArr = inputs.year20day2.trim().split('\n');
   // loop over input array
   inputArr.forEach(ele => {
     // parsing password Rule line into usable parts
     const parsedStringObj = parsePasswordString(ele);
     // putting those into checkValidity function. if valid returns true and validPassword counter goes up
-    if (checkPasswordValidity1(parsedStringObj)) {
-      validPwCount1++;
-    }
-    if (checkPasswordValidity2(parsedStringObj)) {
-      validPwCount2++;
-    }
+    if (checkPasswordValidity1(parsedStringObj)) validPwCount1++;
+    if (checkPasswordValidity2(parsedStringObj)) validPwCount2++;
   });
   // reporting number of valid passwords
   console.log(
@@ -72,3 +70,74 @@ export default function () {
     `In the corrupted file are ${validPwCount2} valid Passwords present for ruleset 2`
   );
 }
+
+// tests
+// put into same file because too lazy right now
+const testing = () => {
+  // test parse String
+  // deepEqual because object values are compared instead of object
+  assert.deepStrictEqual(
+    parsePasswordString('5-17 z: knzcjlgjlkvglrsqzwt'),
+    {
+      pwRangeArr: [5, 17],
+      charToCheck: 'z',
+      password: 'knzcjlgjlkvglrsqzwt',
+    },
+    'failed to parse rule-pw-string'
+  );
+
+  // test validityRule1
+  assert.strictEqual(
+    checkPasswordValidity1({
+      pwRangeArr: [1, 3],
+      charToCheck: 'a',
+      password: 'abcde',
+    }),
+    true,
+    'rule1 fail'
+  );
+
+  // test validityRule2
+  assert.strictEqual(
+    checkPasswordValidity2({
+      pwRangeArr: [1, 3],
+      charToCheck: 'a',
+      password: 'abcde',
+    }),
+    true,
+    'rule2 fail 1'
+  );
+
+  assert.strictEqual(
+    checkPasswordValidity2({
+      pwRangeArr: [4, 13],
+      charToCheck: 'j',
+      password: 'sjjjjjjjjjjjtjj',
+    }),
+    true,
+    'rule2 fail 2'
+  );
+
+  //2-9 c: ccccccccc
+  assert.strictEqual(
+    checkPasswordValidity2({
+      pwRangeArr: [2, 9],
+      charToCheck: 'c',
+      password: 'ccccccccc',
+    }),
+    false,
+    'rule2 fail 3'
+  );
+
+  assert.strictEqual(
+    checkPasswordValidity2({
+      pwRangeArr: [17, 18],
+      charToCheck: 'x',
+      password: 'xxxxxxxxxxxxxxxxkxx',
+    }),
+    true,
+    'rule2 fail 4'
+  );
+};
+
+testing();
